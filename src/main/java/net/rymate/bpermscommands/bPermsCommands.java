f4f0b41065cb35775236e873630831d7a0ad00f8
rymate1234 <supermarioryan@gmail.com>
2012-09-10 19:22:29 +0100

    Yay skeleton plugin

---
+++ src/main/java/net/rymate/bpermscommands/bPermsCommands.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.rymate.bpermscommands;
+
+import java.util.logging.Logger;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author rymate
+ */
+public class bPermsCommands extends JavaPlugin {
+
+    protected final static Logger logger = Logger.getLogger("Minecraft");
+    
+    @Override
+    public void onEnable() {
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+    
+}
