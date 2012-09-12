8a51d083001cb5b3d134a2f83149377a9b66bad6
rymate1234 <supermarioryan@gmail.com>
2012-09-12 21:30:09 +0100

    ...

--- src/main/java/net/rymate/bpermscommands/bApi.java
+++ src/main/java/net/rymate/bpermscommands/bApi.java
@@ -2,6 +2,9 @@ package net.rymate.bpermscommands;
 
 import de.bananaco.bpermissions.api.*;
 import de.bananaco.bpermissions.api.util.CalculableType;
+import java.util.List;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 /*
 * Methods for interacting with the bPermissions api.
 * 
@@ -15,12 +18,15 @@ public class bApi {
      * Checks if the player can modify a group/user. If the priority of that 
      * person is higher, they can't modify it. 
      */
-    public boolean canModify(String sender, String target) {
-        return canModify(sender, target, null);
-    }
+//    public boolean canModify(String sender, String target) {
+//        List<World> wlist = Bukkit.getServer().getWorlds();
+//        for (int i = 0; i < wlist.size(); i++) {
+//            return canModify(sender, target, null);
+//        }
+//    }
     
     public boolean canModify(String sender, String target, String world) {
-        //String priority = ApiLayer.getValue(null, CalculableType.USER, target, "priority");
+        String priority = ApiLayer.getValue(world, CalculableType.USER, target, "priority");
         return true;
     }
     
